b2a5c24fd83fc1dbbdf7f36e48b1b085c153126e
AlanTsarakov <alantsarakov@vk.com>
2026-02-05 01:35:22 +0300

    Добавил в проект модель YandexGptClient.

---
+++ OssetianVerbsTelegramBot/Models/YandexGptModel/YandexGptResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OssetianVerbsTelegramBot.Models.YandexGptModel
+{
+    internal class YandexGptResponse
+    {
+        [JsonPropertyName("output")]
+        public string Output { get; set; } // ← ГЛАВНОЕ ПОЛЕ!
+
+        [JsonPropertyName("model_used")]
+        public string ModelUsed { get; set; }
+
+        [JsonPropertyName("tokens_used")]
+        public int TokensUsed { get; set; }
+    }
+}
